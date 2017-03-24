00ac5c7f9e38b9688c2e76df130f79681f16a81c
Raj <rajkumar.kothandaraman@klm.com>
2017-03-24 16:38:22 +0100

    Added Zoom Animation for the Images

--- RequestResponseMapper/ViewController.swift
+++ RequestResponseMapper/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     var reachability: Reachability?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Users"
@@ -23,10 +23,10 @@ class ViewController: UIViewController {
         } catch {
             print("Unable to start notifier")
         }
-        
+
         NotificationCenter.default.addObserver(self, selector: #selector(ViewController.reachabilityChanged(_:)), name: ReachabilityChangedNotification, object: reachability)
     }
-    
+
     deinit {
         NotificationCenter.default.removeObserver(self, name: ReachabilityChangedNotification, object: nil)
     }
@@ -53,12 +53,41 @@ extension ViewController: UITableViewDataSource {
         userInfoCell.selectionStyle = .none
 
         if let appData = APIController.sharedInstance.appInfo?[indexPath.section].userItem?[indexPath.row] {
-            userInfoCell.name.text = appData.name
+            userInfoCell.name.text = appData.name!+" (\(appData.role!))"
             userInfoCell.email.text = appData.email
             userInfoCell.avatar.image(fromUrl: appData.photo)
+
+            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.imagePressed(_:)))
+
+            userInfoCell.avatar.isUserInteractionEnabled = true
+            userInfoCell.avatar.tag = indexPath.row
+            userInfoCell.avatar.addGestureRecognizer(tapGestureRecognizer)
         }
 
         return userInfoCell
+    }
+
+    func imagePressed(_ sender: UITapGestureRecognizer) {
+
+        let overlay: UIView = UIView(frame: CGRect(x: 0, y: 50, width: self.view.frame.size.width, height: self.view.frame.size.height))
+
+        overlay.backgroundColor = UIColor.black
+
+        let imageView: UIImageView = UIImageView(image: (sender.view as! UIImageView).image) // This includes your image in table view cell
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: 0, y: 70, width: self.view.frame.size.width, height: 500)
+        let doneBtn: UIButton = UIButton(frame: CGRect(x: self.view.frame.size.width - 70, y: 18, width: 75, height: 75))
+        doneBtn.setTitle("Close", for: .normal)
+        doneBtn.setTitleColor(.red, for: .normal)
+        doneBtn.addTarget(self, action: #selector(ViewController.removeView(sender:)), for: .touchUpInside)
+
+        overlay.addSubview(imageView)
+        overlay.addSubview(doneBtn)
+        self.view.addSubview(overlay)
+    }
+
+    func removeView(sender: UIButton!) {
+        sender.superview?.removeFromSuperview()
     }
 }
 
@@ -70,7 +99,7 @@ extension ViewController: UITableViewDelegate {
 }
 
 extension ViewController {
-    func loadUserInformation(){
+    func loadUserInformation() {
         // Do any additional setup after loading the view, typically from a nib.
         APIController.sharedInstance.listUser("members.php") { (error) in
             if error == nil {
@@ -82,32 +111,32 @@ extension ViewController {
             }
         }
     }
-    
+
 }
 
 
 extension ViewController {
-/**
+    /**
  Check for Reachability
  
  - parameter note: post notification object to check for mode of reachability
  */
-func reachabilityChanged(_ note: Notification) {
-    
-    let reachability = note.object as! Reachability
-    
-    if reachability.isReachable {
-        self.loadUserInformation()
-        self.showNoInternetViewController()
-    } else {
-        // Not Reachable
-        if (self.visbileViewController().isKind(of: ViewController.self)) {
-            let next = self.storyboard?.instantiateViewController(withIdentifier:"NoInternet" ) as! NoInternetConnectionController
-            self.present(next, animated: true, completion: nil)
-       }
+    func reachabilityChanged(_ note: Notification) {
+
+        let reachability = note.object as! Reachability
+
+        if reachability.isReachable {
+            self.loadUserInformation()
+            self.showNoInternetViewController()
+        } else {
+            // Not Reachable
+            if (self.visbileViewController().isKind(of: ViewController.self)) {
+                let next = self.storyboard?.instantiateViewController(withIdentifier: "NoInternet") as! NoInternetConnectionController
+                self.present(next, animated: true, completion: nil)
+            }
+        }
     }
-}
-    
+
     /**
      Show No Internet View
      */
@@ -116,7 +145,7 @@ func reachabilityChanged(_ note: Notification) {
             dismiss(animated: true, completion: nil)
         }
     }
-    
+
 }
 
 
